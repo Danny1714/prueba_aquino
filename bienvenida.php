7cdddf295d0c5ef0b0cde54bf62fd8e55b4227ce
Danny1714 <daniel93herna@hotmail.com>
2016-09-28 13:27:51 -0500

    bienvenida
    bienvenida

---
+++ bienvenida.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Myfirst web page with PHP</title>
+	</head>
+
+	<body>
+		<?php
+			echo "Welcome to my first web page with PHP";
+
+		?>
+	</body>
+</html>
+
+
+
